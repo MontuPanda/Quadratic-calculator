81a54dcd93795e03d20662e903f2c3a00a63031a
MontuPanda <75202020+MontuPanda@users.noreply.github.com>
2023-03-23 19:15:17 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,42 @@
+def isfloat(num):
+    try:
+        float(num)
+        return True
+    except ValueError:
+        return False
+while True:
+    a=(input("enter coefficient of x^2:"))
+    if not isfloat(a):
+        print("Invalid")
+        continue
+    else:
+        a=float(a)
+        break
+while True:
+    b=(input("enter coefficient of x:"))
+    if not isfloat(b):
+        print("Invalid")
+        continue
+    else:
+        b=float(b)
+        break
+while True:
+    c=(input("enter constant:"))
+    if not isfloat(c):
+        print("Invalid")
+        continue
+    else:
+        c=float(c)
+        break 
+dis=(b*b)-(4*a*c)
+if dis<0:
+    print("No x-intercepts")
+elif dis==0:
+    rt=dis**(1/2)
+    ans=-(b)/(2*a)
+    print(f"There is one x-intercept : {ans}")
+else:
+    rt=dis**(1/2)
+    posans=(-(b)+rt)/(2*a)
+    negans=(-(b)-rt)/(2*a)
+    print(f"There are 2 x=intercepts at : {posans} and {negans}")
